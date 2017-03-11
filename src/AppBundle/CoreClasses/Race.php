225ed606861e1db16f83e8490ef8b8c0499bfe2d
dykega <gdykema@gmail.com>
2017-03-11 13:52:38 -0600

    Added Race class

---
+++ src/AppBundle/CoreClasses/Race.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\CoreClasses\;
+
+class Race
+{
+    Public $Name;
+    Public $AbilityScoreModifiers;
+    Public $Size;
+    Public $Speed;
+    Public $SpecialAbilities;
+    Public $Languages;
+}
